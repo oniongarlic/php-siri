3a08489b6dcb1c86630ac4c31131553e556dcd97
Kaj-Michael Lang <milang@tal.org>
2015-11-04 16:27:34 +0200

    Adjust example

--- examples/test.php
+++ examples/test.php
@@ -7,7 +7,7 @@ $c=new SiriClient('http://data.foli.fi/siri/');
 $c->loadStops();
 $c->loadStop(1170);
 $vmr=$c->loadVehicles();
-if ($vmr==false)
+if ($vmr===SiriClient::VM_PENDING)
 	echo "VM data is pending\n";
 
 ?>
