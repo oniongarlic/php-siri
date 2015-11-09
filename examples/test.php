d78826d3edf8ee766a9771adcda0c51f0e97c4c4
Kaj-Michael Lang <milang@tal.org>
2015-11-09 21:37:44 +0200

    Test getStop() and print out data

--- examples/test.php
+++ examples/test.php
@@ -5,7 +5,12 @@ require_once('../lib/SiriClient.php');
 $c=new SiriClient('http://data.foli.fi/siri/');
 
 $c->loadStops();
-$c->loadStop(1170);
+$c->loadStop("1170");
+
+$data=$c->getStop("1170");
+
+print_r($data);
+
 $vmr=$c->loadVehicles();
 if ($vmr===SiriClient::VM_PENDING)
 	echo "VM data is pending\n";
